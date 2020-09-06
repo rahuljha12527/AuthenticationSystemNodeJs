789fd7f97fc13cb9176930ace7096dab71f95fd3
rahuljha661 <rahuljha12527@gmail.com>
2020-09-07 01:07:16 +0530

    commit file

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router();
+
+const usersController=require('../controllers/usersController');
+const { route } = require('.');
+
+router.get('/profile',usersController.profile);
+
+
+module.exports=router;
