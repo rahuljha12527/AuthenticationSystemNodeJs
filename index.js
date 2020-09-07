8dc23152d49870a675b7f0896a3c9a1958f4334d
rahuljha661 <rahuljha12527@gmail.com>
2020-09-08 00:05:55 +0530

    create a mongo db database

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 
 const port = 8002;
 const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+
+app.use(express.static('./assets'));
+
 
 
 app.use(expressLayouts);
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
 // use express router
 app.use('/',require('./routes'));
 
