6ae95f5a5c4e8fc10d226ef6e70847bc6092a48f
rahuljha661 <rahuljha12527@gmail.com>
2020-09-08 00:34:06 +0530

    create a sign up page

---
+++ models/singIn.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose');
+
+const signInSchema=new mongoose.Schema({
+    
+    email:{
+        type:String,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+});
+
+const signIn=mongoose.model('SignIn',signInSchema);
+
+module.exports=signIn
