63dcd970451608eab935ec86436d7204e35ada6e
rahuljha661 <rahuljha12527@gmail.com>
2020-09-08 00:48:33 +0530

    create a sign up page

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,21 @@
-module.exports.profile=function(req,res){
-   return res.render('user_profile',{
-     title:"user_profile"  
-   }
-   )
-}
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "user_profile",
+  });
+};
+
+// render the sign up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Sign up",
+  });
+};
+
+// render the sign in page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Sign in",
+  });
+};
+
+
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,9 @@ const { route } = require('.');
 
 router.get('/profile',usersController.profile);
 
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
+
 
 module.exports=router;
