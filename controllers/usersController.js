05c86fce03cfd52c74d0c97c74f4f88d4ad20961
rahuljha661 <rahuljha12527@gmail.com>
2020-09-09 00:00:39 +0530

    commit a file

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
   return res.render("user_profile", {
     title: "user_profile",
@@ -18,4 +20,27 @@ module.exports.signIn = function (req, res) {
   });
 };
 
+// get the sign up data
+module.exports.create = function(req, res){
+  if (req.body.password != req.body.confirm_password){
+      return res.redirect('back');
+  }
+  
+  User.findOne({email: req.body.email}, function(err, user){
+      if(err){console.log('error in finding user in signing up'); return}
 
+      if (!user){
+          User.create(req.body, function(err, user){
+              if(err){console.log('error in creating user while signing up'); return}
+
+              return res.redirect('/users/sign-in');
+          })
+      }else{
+          return res.redirect('back');
+      }
+
+  });
+}
+
+// sing in create a sessition for the user
+module.exports.createSesition = function (req, res) {};
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 const port = 8002;
-const expressLayouts=require('express-ejs-layouts');
-const db=require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
+app.use(express.urlencoded());
 
-app.use(express.static('./assets'));
+app.use(cookieParser());
 
-
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // use express router
-app.use('/',require('./routes'));
+app.use("/", require("./routes"));
 
 // set up the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
-
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
@@ -30,5 +30,4 @@ app.listen(port, function (err) {
   }
 
   console.log(`Server is running on port: ${port} `);
-
 });
